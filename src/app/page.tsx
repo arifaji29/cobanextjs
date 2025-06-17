52be56099c165973618c7d844b3c882482555578
arifaji29 <arifajif@gmail.com>
2025-06-18 02:06:43 +0700

    oke 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image';
 import {Navbar} from '@/app/components/Navbar'; 
+import { HeroSection } from '@/app/components/HeroSection';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      {/* Sisa section landing page akan ada di sini */}
-      <h1 className="mt-20">Selamat Datang!</h1> {/* mt-20 untuk memberi jarak dari navbar fixed */}
+      <HeroSection />
+      {/* <FeaturesSection /> */}
+      {/* <Footer /> */}
     </main>
   )
 }
