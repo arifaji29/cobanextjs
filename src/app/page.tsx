7164901d56685d55e1eeee99802727df3a55c5d2
arifaji29 <arifajif@gmail.com>
2025-06-18 21:29:37 +0700

    ya1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@ import Image from 'next/image';
 import {Navbar} from '@/app/components/Navbar'; 
 import { HeroSection } from '@/app/components/HeroSection';
 
+
+
 export default function Home() {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <Image
+  src="/mylogo.png" // Path dimulai dari folder public
+  alt="Deskripsi gambar"
+  width={500}
+  height={300}
+  className="rounded-lg"
+/>
       {/* <FeaturesSection /> */}
       {/* <Footer /> */}
     </main>
